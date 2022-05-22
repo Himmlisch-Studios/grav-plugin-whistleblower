0dd19de3d3e7002909594cc90cf7f3fd04e8246f
Luan Himmlisch <luan@himmlisch.com.mx>
2022-05-22 03:24:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Whistleblower Plugin
+
+The **Whistleblower** Plugin is an extension for [Grav CMS](https://github.com/getgrav/grav).
+
+**Whistleblower** helps you to know what happens on your site everytime by notifying you directly to your phone.
+
+## Installation
+
+Installing the Whistleblower plugin can be done in one of three ways: The GPM (Grav Package Manager) installation method lets you quickly install the plugin with a simple terminal command, the manual method lets you do so via a zip file, and the admin method lets you do so via the Admin Plugin.
+
+### GPM Installation (Preferred)
+
+To install the plugin via the [GPM](https://learn.getgrav.org/cli-console/grav-cli-gpm), through your system's terminal (also called the command line), navigate to the root of your Grav-installation, and enter:
+
+    bin/gpm install whistleblower
+
+This will install the Whistleblower plugin into your `/user/plugins`-directory within Grav. Its files can be found under `/your/site/grav/user/plugins/whistleblower`.
+
+### Manual Installation
+
+To install the plugin manually, download the zip-version of this repository and unzip it under `/your/site/grav/user/plugins`. Then rename the folder to `whistleblower`. You can find these files on [GitHub](https://github.com/himmlisch-studios/grav-plugin-whistleblower) or via [GetGrav.org](https://getgrav.org/downloads/plugins).
+
+You should now have all the plugin files under
+
+    /your/site/grav/user/plugins/whistleblower
+
+### Admin Plugin
+
+If you use the Admin Plugin, you can install the plugin directly by browsing the `Plugins`-menu and clicking on the `Add` button.
+
+## Configuration
+
+Before configuring this plugin, you should copy the `user/plugins/whistleblower/whistleblower.yaml` to `user/config/plugins/whistleblower.yaml` and only edit that copy.
+
+Here is the default configuration and an explanation of available options:
+
+```yaml
+enabled: true
+admin_mode: true
+endpoint: ""
+events:
+    onUserLogin: "User '{username}' has logged on {domain}!"
+    onUserLoginFailure: "Someone tried to log on {domain}!"
+```
+
+Note that if you use the Admin Plugin, a file with your configuration named whistleblower.yaml will be saved in the `user/config/plugins/`-folder once the configuration is saved in the Admin.
+
+## Usage
+
+To start using the plugin you need to setup a ntfy endpoint. **Make sure this is unique**, since anyone could guess your endpoint and listen for all notifications.
+
+Once setup, you can start adding any event hooks from Grav or Grav plugins and pairing them with a message you want to receive.
+
+You can use a variety of variables to customize your messages. Such as `{username}` to receive the user who executed the event.
+
+To start listening to the notifications you can either install the NTFY app available at the [F-Droid Store](https://f-droid.org/en/packages/io.heckel.ntfy/) or the [Google Play Store](https://play.google.com/store/apps/details?id=io.heckel.ntfy).
+
+Or you can choose to use the [Web App](https://ntfy.sh/app).
+
+To finish, just add the exact endpoint/topic you setup on the plugin configuration.
+
+## Credits
+
+Thanks to [Philip C. Heckel](https://heckel.io/) and the contributors of [NTFY](https://github.com/binwiederhier/ntfy) for making this service publicly available and free.
+
+## Need a website?
+
+At [Himmlisch Web](https://web.himmlisch.com.mx) we help you to develop the site of your dreams, with the exact functionality and look as you want. [Contact us!](https://himmlisch.com.mx/contact)
---
+++ whistleblower.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Grav\Plugin;
+
+use Composer\Autoload\ClassLoader;
+use Grav\Common\Plugin;
+use Grav\Common\Utils;
+
+/**
+ * Class WhistleblowerPlugin
+ * @package Grav\Plugin
+ */
+class WhistleblowerPlugin extends Plugin
+{
+    /**
+     * @return array
+     *
+     * The getSubscribedEvents() gives the core a list of events
+     *     that the plugin wants to listen to. The key of each
+     *     array section is the event that the plugin listens to
+     *     and the value (in the form of an array) contains the
+     *     callable (or function) as well as the priority. The
+     *     higher the number the higher the priority.
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'onPluginsInitialized' => [
+                ['onPluginsInitialized', 0]
+            ]
+        ];
+    }
+
+    /**
+     * Composer autoload
+     *
+     * @return ClassLoader
+     */
+    public function autoload(): ClassLoader
+    {
+        return require __DIR__ . '/vendor/autoload.php';
+    }
+
+    /**
+     * Initialize the plugin
+     */
+    public function onPluginsInitialized(): void
+    {
+        $this->getConfigs();
+
+        if ($this->config['admin_only'] && !$this->isAdmin() || empty($this->configs['endpoint'])) {
+            return;
+        }
+
+        $this->events = [];
+
+        $site = $this->grav['config']->get('site.title') ?? 'Grav Website';
+        $user = $this->grav['user'];
+        $server = $this->configs['custom_server'] ?? 'https://ntfy.sh';
+        $server = !empty($server) ? $server : 'https://ntfy.sh';
+        $variables = [
+            '{site}' => $site,
+            '{domain}' => $this->grav['uri']->rootUrl(true) ?? '',
+            '{username}' => $user !== null ? $user->get('username') ?? '' : '',
+            '{date}' => date('m.d.y'),
+            '{hour}' => date('g:i A')
+        ];
+
+        foreach ($this->configs['events'] as $event => $message) {
+            $message = str_replace(
+                array_keys($variables),
+                $variables,
+                $message
+            );
+
+            $header = [];
+            $header['Title'] = $site;
+
+            if (isset($this->configs['custom_password']) && !empty($this->configs['custom_password'])) {
+                $header['Authorization'] = "Basic " . $this->configs['custom_password'];
+            }
+
+            $this->events[$event] = fn () => $this->ntfy($this->configs['endpoint'], $message, $header, $server);
+
+            $this->enable([
+                $event => [
+                    ['executeEvent', $event]
+                ]
+            ]);
+        }
+    }
+
+    public function executeEvent(mixed $_, String $key): void
+    {
+        call_user_func($this->events[$key]);
+    }
+
+    private function getConfigs(): void
+    {
+        $this->configs = $this->config->get('plugins.whistleblower');
+    }
+
+    private function ntfy(
+        String $endpoint,
+        String $content,
+        array $header = ['Title' => 'Whistleblower ~'],
+        String $server = "https://ntfy.sh",
+        int $priority = 3
+    ): void {
+        if (empty($endpoint)) {
+            return;
+        }
+        array_walk($header, fn (&$v, $key) => $v = "$key: $v");
+
+        $ch = curl_init("$server/$endpoint");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array_merge([
+            "Content-Type: text/plain",
+            "Priority: $priority"
+        ], $header));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+
+        curl_exec($ch);
+        curl_close($ch);
+    }
+}
